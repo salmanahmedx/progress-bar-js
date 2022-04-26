06e732a3a326e1bd9fabde3fe7a3d13e0feeba83
Salman Ahmed <salmanahmedx@hotmail.com>
2022-04-26 09:54:38 +0600

    previous btn functionality

--- script.js
+++ script.js
@@ -11,36 +11,29 @@ buttons.forEach(btn => {
 
             console.log(count)
 
-            for (let i = 1; i < count + 1; i++) {
-                bars.forEach(bar => {
-                    if (bar.classList.contains(`bar-${i}`)) {
-                        let currBar = document.querySelector(`.bar-${count}`)
-                        currBar.style.backgroundColor = 'green';
-                    }
-                })
-            }
-
+            // Updating progress bar - Next 
+            bars.forEach(bar => {
+                if (bar.classList.contains(`bar-${count}`)) {
+                    let currBar = document.querySelector(`.bar-${count}`)
+                    currBar.style.backgroundColor = 'green';
+                }
+            })
 
         } else if (btnClass.contains('previous-btn')) {
-            if (count > 1) {
+            if (count !== 0) {
                 count--;
             }
 
             console.log(count)
 
-            for (let i = count; i > 0; i--) {
-                bars.forEach(bar => {
-                    if (bar.classList.contains(`bar-${i}`)) {
-                        let currBar = document.querySelector(`.bar-${count}`)
-                        currBar.style.backgroundColor = 'transparent';
-                    }
-                })
-            }
+            // Updating progress bar - Previous
+
+            bars.forEach(bar => {
+                if (bar.classList.contains(`bar-${count + 1}`)) {
+                    let currBar = document.querySelector(`.bar-${count + 1}`)
+                    currBar.style.backgroundColor = 'transparent';
+                }
+            })
         }
-
-
-
-
-
     })
 })
