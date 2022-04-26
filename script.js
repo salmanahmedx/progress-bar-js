624de4f89dd5bfc2e9a778a05831ce914b6ecd68
Salman Ahmed <salmanahmedx@hotmail.com>
2022-04-26 10:13:10 +0600

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll(".btn")
 const bars = document.querySelectorAll(".bar")
+const progressBars = document.querySelector(".progress-bar")
 
 let count = 0;
 
@@ -15,9 +16,13 @@ buttons.forEach(btn => {
             bars.forEach(bar => {
                 if (bar.classList.contains(`bar-${count}`)) {
                     let currBar = document.querySelector(`.bar-${count}`)
-                    currBar.style.backgroundColor = 'green';
+                    currBar.style.borderColor = 'green';
+                    currBar.style.color = 'green';
                 }
             })
+
+            // progress bar update
+            progressBars.style.width = `${(count / 5) * 100}%`
 
         } else if (btnClass.contains('previous-btn')) {
             if (count !== 0) {
@@ -31,9 +36,13 @@ buttons.forEach(btn => {
             bars.forEach(bar => {
                 if (bar.classList.contains(`bar-${count + 1}`)) {
                     let currBar = document.querySelector(`.bar-${count + 1}`)
-                    currBar.style.backgroundColor = 'transparent';
+                    currBar.style.borderColor = 'black';
+                    currBar.style.color = 'black';
                 }
             })
+
+            // progress bar update
+            progressBars.style.width = `${(count / 5) * 100}%`
         }
     })
 })
